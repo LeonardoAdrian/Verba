5a9fa00bfcddbbf5f30e3f1727c7fe543e5b7fe1
Leonardo <leonardo.salinas@intisoluciones.com>
2025-02-21 12:10:27 -0500

    hola

--- goldenverba/components/generation/AzureOpenAIGenerator.py
+++ goldenverba/components/generation/AzureOpenAIGenerator.py
@@ -6,15 +6,11 @@ from goldenverba.components.types import InputConfig
 from goldenverba.components.util import get_environment, get_token
 import httpx
 import json
-<<<<<<< HEAD
 from typing import List
 import requests
-=======
 import re
-import base64
 from psycopg2 import sql as psql
 
->>>>>>> origin/test
 
 load_dotenv()
 
@@ -139,8 +135,7 @@ class AzureOpenAIGenerator(Generator):
                             if "delta" in choice and "content" in choice["delta"]:
                                 full_response += choice["delta"]["content"]
                             elif "finish_reason" in choice:
-                                pass
-        print(full_response)                            
+                                pass                           
         
         sql_query = self.extract_sql_query(full_response)
         if sql_query:
@@ -185,29 +180,7 @@ class AzureOpenAIGenerator(Generator):
             
         azure_url = f"https://{resource_name}.openai.azure.com"
         version = config.get("VERSION", {"value": "2024-08-01-preview"}).value
-<<<<<<< HEAD
-=======
-        if "imagen" in query.lower() or "img/" in context.lower():
-            image_path = self.find_image_in_folder(query, context)
-            if image_path:
-                # Leer la imagen y convertirla a base64
-                with open(image_path, "rb") as image_file:
-                    encoded_image = base64.b64encode(image_file.read()).decode("utf-8")
                 
-                # Agregar la imagen al contexto
-                context += f"\n\n[Imagen encontrada: <img src='data:image/png;base64,{encoded_image}' alt='Imagen relacionada' />]"
-                print(f'Contexto actualizado con imagen: {context}')
-            else:
-                context += "\n\nNo se encontró ninguna imagen que coincida con tu búsqueda."
-                print(f'Contexto actualizado: {context}')
-        database = config.get("DATABASE").value
-        if database:
-            async for result in self.metodo_previo(query, config, context, conversation):
-                if result.get("finish_reason") == "stop" and result.get("message"):
-                    context = f"\n\nSegun la base de datos esta es la respuesta con los datos mas recientes que responde el query del usuario:\n{str(result['message'])}"
-                    print(f'Contexto actualizado: {context}')
-                
->>>>>>> origin/test
         messages = self.prepare_messages(query, context, conversation, system_message)
         headers = {
             "Content-Type": "application/json",
@@ -218,10 +191,8 @@ class AzureOpenAIGenerator(Generator):
             "model": model,
             "stream": True,
         }
-        print("AQUI ESTAMOS --->" + os.getcwd())
-        
-        
-        
+        print(f"Requesting to {azure_url}/openai/deployments/{model}/chat/completions?api-version={version}")
+        print(f"Prompt: {data}")
         
         async with httpx.AsyncClient() as client2:
             async with client2.stream(
@@ -299,7 +270,7 @@ class AzureOpenAIGenerator(Generator):
         messages.append(
             {
                 "role": "user",
-                "content": f"Answer this query: '{query}' with this provided context: {context}. Add these images to the answer /imgs/thumbnail.png, /imgs/api_screen.png",
+                "content": f"Answer this query: '{query}' with this provided context: {context}",
             }
         )
 
