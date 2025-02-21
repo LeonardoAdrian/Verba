1f746b7db25667f1c4fcfa76ef3d49cc456e5064
Leonardo <leonardo.salinas@intisoluciones.com>
2025-02-21 17:57:01 -0500

    Reemplazado logo y titulo de pagina

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Verba",
-  description: "The GoldenRAGtriever",
+  title: "IntiApp",
+  description: "RAG App",
 };
 
 export default function RootLayout({
@@ -13,8 +13,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <link rel="icon" href="icon.ico" />
-      <link rel="icon" href="static/icon.ico" />
+      <link rel="icon" href="inti_logo.png" />
+      <link rel="icon" href="static/inti_logo.png" />
       <body>{children}</body>
     </html>
   );
